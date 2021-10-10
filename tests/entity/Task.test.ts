675b7a1adc96306f3ab73dbc54a83f7a1d38836d
Ethan Lane <ethan@vylpes.com>
2021-10-10 14:48:18 +0100

    Task.CreateTask tests

---
+++ tests/entity/Task.test.ts
@@ -0,0 +1,187 @@
+import { mock } from "jest-mock-extended";
+
+const connectionMock = mock<Connection>();
+const qbuilderMock = mock<SelectQueryBuilder<any>>();
+
+let repositoryMock = mock<Repository<any>>();
+let taskMock = mock<Task>();
+
+jest.mock('typeorm', () => {
+    qbuilderMock.where.mockReturnThis();
+    qbuilderMock.select.mockReturnThis();
+    repositoryMock.createQueryBuilder.mockReturnValue(qbuilderMock);
+    repositoryMock.findOne.mockImplementation(async () => {
+	return taskMock;
+    });
+    connectionMock.getRepository.mockReturnValue(repositoryMock);
+
+    return {
+	getConnection: () => connectionMock,
+
+	BaseEntity: class Mock {},
+	ObjectType: () => {},
+	Entity: () => {},
+	InputType: () => {},
+	Index: () => {},
+	PrimaryColumn: () => {},
+	Column: () => {},
+	CreateDateColumn: () => {},
+	UpdateDateColumn: () => {},
+	OneToMany: () => {},
+	ManyToOne: () => {},
+    }
+});
+
+import { Connection, Repository, SelectQueryBuilder } from "typeorm";
+import { UserProjectRole } from "../../src/constants/UserProjectRole";
+import { Project } from "../../src/entity/Project";
+import { ProjectUser } from "../../src/entity/ProjectUser";
+import { User } from "../../src/entity/User";
+import { Task } from "../../src/entity/Task";
+
+beforeEach(() => {
+    // Repository Mock
+    repositoryMock = mock<Repository<any>>();
+
+    repositoryMock.createQueryBuilder.mockReturnValue(qbuilderMock);
+    repositoryMock.findOne.mockImplementation(async () => {
+	return taskMock;
+    });
+    connectionMock.getRepository.mockReturnValue(repositoryMock);
+
+    // Task Mock
+    taskMock = mock<Task>();
+});
+
+describe('Constructor', () => {
+    test('Expect properties are set', () => {
+	const date = new Date();
+	const user = {} as unknown as User;
+	const project = mock<Project>();
+	const parentTask = mock<Task>();
+
+	const task = new Task('taskId', 1, 'name', 'description', user, date, false, false, project,
+	user, parentTask);
+
+	expect(task.Id).toBe('taskId');
+	expect(task.TaskNumber).toBe(1);
+	expect(task.Name).toBe('name');
+	expect(task.Description).toBe('description');
+	expect(task.CreatedBy).toBe(user);
+	expect(task.CreatedAt).toBe(date);
+	expect(task.Done).toBeFalsy();
+	expect(task.Archived).toBeFalsy();
+	expect(task.Project).toBe(project);
+	expect(task.AssignedTo).toBe(user);
+	expect(task.ParentTask).toBe(parentTask);
+    });
+});
+
+describe('GetAllTasks', () => {
+    test('Expect all visible tasks to be returned', async () => {
+	const currentUser = mock<User>();
+	currentUser.Id = 'userId';
+
+	const project1 = mock<Project>();
+
+	const project2 = mock<Project>();
+
+	const task1A = mock<Task>();
+	task1A.AssignedTo = currentUser;
+	task1A.Project = project1;
+
+	const task1B = mock<Task>();
+	task1B.AssignedTo = currentUser;
+	task1B.Project = project1;
+
+	const task2A = mock<Task>();
+	task2A.AssignedTo = currentUser;
+	task2A.Project = project2;
+
+	const projectUser1 = mock<ProjectUser>();
+	projectUser1.User = currentUser;
+	projectUser1.Project = project1;
+
+	const projectUser2 = mock<ProjectUser>();
+	projectUser2.User = currentUser;
+	projectUser2.Project = project2;
+
+	project1.Tasks = [ task1A, task1B ];
+	project2.Tasks = [ task2A ];
+
+	currentUser.AssignedProjects = [ projectUser1, projectUser2 ];
+
+	repositoryMock.findOne.mockResolvedValue(currentUser);
+
+	const result = await Task.GetAllTasks(currentUser);
+
+	expect(result.length).toBe(3);
+	expect(result).toContain(task1A);
+	expect(result).toContain(task1B);
+	expect(result).toContain(task2A);
+    });
+});
+
+describe('GetAssignedTasks', () => {
+    test('Given user exists, expect tasks returned', async () => {
+	const task = mock<Task>();
+
+	const user = mock<User>();
+	user.Id = 'userId';
+	user.AssignedTasks = [ task ];
+
+	repositoryMock.findOne.mockResolvedValue(user);
+
+	const result = await Task.GetAssignedTasks('userId');
+
+	expect(result.length).toBe(1);
+	expect(result).toContain(task);
+    });
+
+    test('Given user does not exist, expect null returned', async () => {
+	repositoryMock.findOne.mockResolvedValue(null);
+
+	const result = await Task.GetAssignedTasks('userId');
+
+	expect(result).toBeNull();
+    });
+});
+
+describe('CreateTask', () => {
+    test('Given user has permission, expect task to be saved', async () => {
+	const user = mock<User>();
+	user.Id = 'userId';
+
+	const project = mock<Project>();
+	project.Id = 'projectId';
+	project.NextTask = 1;
+	
+	Project.GetNextTask = jest.fn().mockImplementation(async (projectId: string, createdBy: User) => {
+	    return project.NextTask++;
+	});
+
+	ProjectUser.HasPermission = jest.fn().mockResolvedValue(true);
+	
+	const result = await Task.CreateTask('name', 'description', user, project, null, null);
+
+	expect(repositoryMock.save).toBeCalledWith(result);
+	expect(result.Project).toBe(project);
+	expect(result.CreatedBy).toBe(user);
+	expect(project.NextTask).toBe(2);
+    });
+
+    test('Given user does not have permission, expect null to be returned', async () => {
+	const user = mock<User>();
+	user.Id = 'userId';
+
+	const project = mock<Project>();
+	project.Id = 'projectId';
+
+	ProjectUser.HasPermission = jest.fn().mockResolvedValue(false);
+
+	const result = await Task.CreateTask('name', 'description', user, project, null, null);
+
+	expect(result).toBeNull();
+	expect(repositoryMock.save).not.toBeCalled();
+    });
+});
