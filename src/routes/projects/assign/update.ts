79aa6b30b4463fc2e55891e2029efa145ba158e5
Ethan Lane <ethan@vylpes.com>
2021-11-03 17:36:07 +0000

    Assign/update tests

--- src/routes/projects/assign/update.ts
+++ src/routes/projects/assign/update.ts
@@ -2,6 +2,7 @@ import { Page } from "../../../contracts/Page";
 import { UserMiddleware } from "../../../middleware/userMiddleware";
 import { Router, Request, Response } from "express";
 import { ProjectUser } from "../../../entity/ProjectUser";
+import { UserProjectPermissions } from "../../../constants/UserProjectRole";
 
 export class Update extends Page {
     constructor(router: Router) {
@@ -10,6 +11,12 @@ export class Update extends Page {
 
     OnGet() {
         super.router.get('/assign/update/:projectId/:userId', UserMiddleware.Authorise, async (req: Request, res: Response) => {
+            if (!(await ProjectUser.HasPermission(req.params.projectId, req.session.User.Id, UserProjectPermissions.Promote))) {
+                req.session.error = "Unauthorised";
+                res.redirect("/projects/list");
+                return;
+            }
+
             const result = await ProjectUser.ToggleAdmin(req.params.projectId, req.params.userId, req.session.User);
 
             if (!result) {
